c519d10e324f95945af3b40c7b9ee034186151b9
Leon Boschman <leon.boschman@gmail.com>
2022-12-06 21:30:26 +0100

    Add day 1 part 2

--- day01/calorie_counting.cpp
+++ day01/calorie_counting.cpp
@@ -2,15 +2,15 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
-int main() {
-
+int part_1() {
     std::string filename = "input.txt";
     std::ifstream input(filename);
 
     int max_elf = 0, current_elf = 0, current_cookie = 0;
     std::string str_cur_cookie;
-    
+
     while (!input.eof()) {
 
         std::getline(input, str_cur_cookie);
@@ -25,9 +25,48 @@ int main() {
         }
     }
 
+    return max_elf;
+}
+
+int part_2() {
+    std::string filename = "input.txt";
+    std::ifstream input(filename);
+
+    int current_elf = 0, current_cookie = 0;
+    std::string str_cur_cookie;
+
+    std::vector<int> all_elves;
+
+    while (!input.eof())
+    {
+        std::getline(input, str_cur_cookie);
+        if (str_cur_cookie.length() > 0) {
+            current_cookie = std::stoi(str_cur_cookie);
+            current_elf += current_cookie;
+        } else {
+            all_elves.push_back(current_elf);
+            current_elf = 0;
+        }
+        
+    }
+
+    std::sort(all_elves.begin(), all_elves.end());
+    std::reverse(all_elves.begin(), all_elves.end());
+    
+    int top_3 = all_elves[0] + all_elves[1] + all_elves[2];
+
+    return top_3;
+    
+}
+
+
+int main() {
+    int max_elf = part_1();
+
+    int max_3 = part_2();
+
     std::cout << max_elf << std::endl;
-
-
+    std::cout << max_3 << std::endl;
 
     return 0;
 }
