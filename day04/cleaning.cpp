12e343679b411dfae05879a817a17a14e177f248
Leon Boschman <leon.boschman@gmail.com>
2022-12-18 17:19:50 +0100

    Solve day 4 part 2

--- day04/cleaning.cpp
+++ day04/cleaning.cpp
@@ -18,6 +18,14 @@ int fully_contained_ranges(cleaning_range range1, cleaning_range range2){
     }
 }
 
+int overlapping_ranges(cleaning_range range1, cleaning_range range2) {
+    if (range1.lower <= range2.upper && range1.upper >= range2.lower) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 cleaning_range make_range(std::string line) {
     char sep = '-';
     std::size_t pos = line.find(sep);
@@ -45,12 +53,26 @@ int parse_line(std::string line) {
 
 }
 
+int parse_line_2(std::string line) {
+    std::string line1, line2;
+    cleaning_range range1, range2;
+
+    std::size_t pos = line.find(',');
+    line1 = line.substr(0, pos);
+    line2 = line.substr(pos+1);
+
+    range1 = make_range(line1);
+    range2 = make_range(line2);
+
+    return overlapping_ranges(range1, range2);
+
+}
 
 
 int part_1() {
     std::ifstream input("input.txt");
     std::string line;
-    int score;
+    int score = 0;
 
     while (getline(input,line))
     {
@@ -61,11 +83,25 @@ int part_1() {
 
 }
 
+int part_2() {
+    std::ifstream input("input.txt");
+    std::string line;
+    int score = 0;
+
+    while (getline(input,line))
+    {
+        score += parse_line_2(line);
+    }
+    
+    return score;
+}
 
 int main() {
     int score_1 = part_1();
-
     std::cout << score_1 << std::endl;
+
+    int score_2 = part_2();
+    std::cout << score_2 << std::endl;
 
     return 0;
 }
