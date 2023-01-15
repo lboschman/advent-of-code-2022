22dc9b24f93211a0d79c8d7b25963710d02a0281
Leon Boschman <leon.boschman@gmail.com>
2023-01-15 21:23:13 +0100

    Add solution for day 11 part 1

--- day11/monkkeybusiness.cpp
+++ day11/monkkeybusiness.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 
 class Monkey {
@@ -16,6 +17,8 @@ class Monkey {
     int test_true_receiver = 0;
     int test_false_receiver = 0;
     std::vector<int> items;
+
+    int inspections = 0;
 
     Monkey(){
       all_monkeys.push_back(*this);
@@ -31,12 +34,36 @@ class Monkey {
 
     int new_worry_level(int old) {
       int intermediate = intermediate_worry_level(old);
+      inspections++;
+      // std::cout << inspections << std::endl;
       int new_worry = intermediate / 3;
       return new_worry;
     }
 
     Monkey* monkey_pointer(int index) {
       return &(all_monkeys[index]);
+    }
+
+    int which_monkey_to_throw_to(int worry_level) {
+      return (worry_level % test_divisor == 0) ? test_true_receiver : test_false_receiver;
+    }
+
+    void throw_item() {
+      int current_item = items[0];
+      int new_worry = new_worry_level(current_item);
+
+      int to_monkey = which_monkey_to_throw_to(new_worry);
+
+      monkey_pointer(to_monkey)->items.push_back(new_worry);
+
+      items.erase(items.begin());
+
+    }
+
+    void throw_all_items() {
+      while (items.size() > 0) {
+        throw_item();
+      }
     }
 };
 
@@ -103,19 +130,39 @@ void part_1() {
     index++;
   }
 
-  for (Monkey m1 : Monkey::all_monkeys) {
-    for (int item : m1.items) {
-      std::cout << item << " ";
+  // for (Monkey m1 : Monkey::all_monkeys) {
+  //   for (int item : m1.items) {
+  //     std::cout << item << " ";
+  //   }
+  //   std::cout << std::endl;
+  //   std::cout << m1.worry_level_additive << ": " << m1.worry_level_multiplier_additive << std::endl;
+  //   std::cout << m1.test_divisor << " ? " << m1.test_true_receiver << " : " << m1.test_false_receiver << std::endl;
+  //   std::cout << "====================" << std::endl;
+  // }
+
+
+  int cycle = 0;
+  int max_cycles = 20;
+
+  while (cycle < max_cycles) {
+    for (int i=0; i<Monkey::all_monkeys.size(); i++) {
+      Monkey::all_monkeys[i].throw_all_items();
+      // m.throw_all_items();
     }
-    std::cout << std::endl;
-    std::cout << m1.worry_level_additive << ": " << m1.worry_level_multiplier_additive << std::endl;
-    std::cout << m1.test_divisor << " ? " << m1.test_true_receiver << " : " << m1.test_false_receiver << std::endl;
-    std::cout << "====================" << std::endl;
+    std::cout << "Finished cycle: " << cycle << std::endl;
+    cycle++;
   }
 
+  std::vector<int> inspections = {};
+  for (Monkey m : Monkey::all_monkeys) {
+    inspections.push_back(m.inspections);
+  }
+
+  std::sort(inspections.begin(), inspections.end(), std::greater<>());
+
+  std::cout << inspections[0] * inspections[1] << std::endl;
+
 }
-
-
 
 int main() {
   part_1();
