ec9203651b0d60fff74fd3660e38097a166e73d1
Leon Boschman <leon.boschman@gmail.com>
2023-02-28 13:35:25 +0100

    Add solution for day 14 part 1

--- day14/rock_air_sand.cpp
+++ day14/rock_air_sand.cpp
@@ -11,6 +11,7 @@ struct RockGrid {
   char air = '.';
   char rock = '#';
   char sand = 'o';
+  int dropped_sand = 0;
 
   std::vector<std::vector<char>> grid = {{air}};
 
@@ -75,6 +76,61 @@ struct RockGrid {
       grid[y][i] = rock;
     }
   }
+
+  int drop_block_sand(int origin_x, int origin_y) {
+    grid[origin_y][origin_x] = sand;
+    int response = move_block_sand(origin_x, origin_y);
+    dropped_sand += (1+response);
+    return response;
+  }
+
+  int move_block_sand(int x, int y) {
+    // return values:
+    // 1: block needs to move
+    // 0: block cannot move, terminate
+    // -1: block has fallen off the grid, terminate
+
+    // overflow condition
+    if (y==grid.size() - 1) {
+      return -1;
+    }
+
+    // drop straight down
+    if (grid[y+1][x]==air) {
+      grid[y][x] = air;
+      grid[y+1][x] = sand;
+      return move_block_sand(x, y+1);
+    // if not straight down, move down-left (problem specification)
+    } else if (grid[y+1][x-1]==air) {
+      grid[y][x] = air;
+      grid[y+1][x-1] = sand;
+      return move_block_sand(x-1, y+1);
+    // if not down-left, move down-right (problem specification)
+    } else if (grid[y+1][x+1]==air) {
+      grid[y][x] = air;
+      grid[y+1][x+1] = sand;
+      return move_block_sand(x+1, y+1);
+    } else {
+      return 0;
+    }
+  }
+
+  void drop_all_sand(int origin_x, int origin_y) {
+    int overflow = 0;
+    while (overflow==0) {
+      overflow = drop_block_sand(origin_x, origin_y);
+    }
+  }
+
+  void print_grid() {
+    for (auto line : grid) {
+      for (char c : line) {
+        std::cout << c;
+      }
+      std::cout << std::endl;
+    }
+  }
+
 };
 
 RockGrid parse_input(std::string fname) {
@@ -87,7 +143,7 @@ RockGrid parse_input(std::string fname) {
 
   while (getline(input, line)) {
     std::stringstream s(line);
-    std::cout << "Parsing line " << i << std::endl;
+    // std::cout << "Parsing line " << i << std::endl;
 
     int x_start, y_start, x_end, y_end;
     x_end = y_end = 0;
@@ -96,7 +152,7 @@ RockGrid parse_input(std::string fname) {
         x_start = x_end;
         y_start = y_end;
         s >> x_end >> comma >> y_end;
-        std::cout << "New Coordinates: " << x_end << ", " << y_end << std::endl;
+        // std::cout << "New Coordinates: " << x_end << ", " << y_end << std::endl;
         if (x_start > 0 && y_start > 0) {
           canyon.add_rock(x_start, x_end, y_start, y_end);
         }
@@ -112,9 +168,9 @@ RockGrid parse_input(std::string fname) {
 }
 
 int main() {
-  RockGrid canyon = parse_input("input_short.txt");
-  std::cout << canyon.grid.size() << std::endl;
-  std::cout << canyon.grid[0].size() << std::endl;
+  RockGrid canyon = parse_input("input.txt");
+  canyon.drop_all_sand(500, 0);
 
-  return 0;
+  std::cout << "Dropped blocks of sand: " << canyon.dropped_sand << std::endl;
+
 }
